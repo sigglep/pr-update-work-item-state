8695f047afa3c9cd9a767ff9bd91389bb64d6715
Marcin Gadomski <marcingadomski94@gmail.com>
2020-12-18 15:59:12 +0100

    logging

--- index.js
+++ index.js
@@ -137,6 +137,7 @@ async function updateWorkItem(workItemId, env) {
                     );
             console.log("Work Item " + workItemId + " state is updated to " + env.closedstate);         
         } else {
+            try {
             console.log("PR IS OPENED");
             mergeStatus = "Linked new Pull Request to Azure Boards";
             newDescription = currentDescription + "<br />" + mergeStatus;
@@ -161,6 +162,9 @@ async function updateWorkItem(workItemId, env) {
                     (validateOnly = false)
                     );
             console.log("Work Item " + workItemId + " state is updated to " + env.propenstate);     
+            } catch (err) {
+                console.log(err);
+            }
         }
 
         return workItemSaveResult;
