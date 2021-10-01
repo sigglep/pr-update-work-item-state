2eaa72c74a836e2281e948660e556c14d6f580a5
Marcin Gadomski <marcingadomski94@gmail.com>
2021-10-01 17:28:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const azureDevOpsHandler = require(`azure-devops-node-api`);
 const core = require(`@actions/core`);
 const github = require(`@actions/github`);
 const fetch = require("node-fetch");
-const version = "1.0.18"
+const version = "1.0.19"
 global.Headers = fetch.Headers;
 
 
